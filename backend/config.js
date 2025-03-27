8ffe8794d8e11861e050edb54c16be26f2833602
sunil-promineo-FE <sunilvaishali43@gmail.com>
2025-03-27 19:42:13 +0530

    tmp commit
    tmp commit

--- backend/config.js
+++ backend/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 3003,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://sunilganotra:desiPwd@8805g@cluster0.mongodb.net/?retryWrites=true&w=majority',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://sunilganotra:desiPwd%408805g@cluster0.mongodb.net/?retryWrites=true&w=majority',
   FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
   API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:3003',
 };
