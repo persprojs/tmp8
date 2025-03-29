9179f2475f14a45295eaab7ad82ac840640cacea
sunil-promineo-FE <sunilvaishali43@gmail.com>
2025-03-29 17:57:36 +0530

    tmp commit
    tmp commit

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -1,2 +1,4 @@
 // src/config/config.js
-export const API_BASE_URL = "http://localhost:3003"; // Hardcode for now
+//export const API_BASE_URL = "http://localhost:3003"; // Hardcode for now
+// Should already work if you have:
+export const API_BASE_URL = process.env.REACT_APP_API_URL + '/api'; 
