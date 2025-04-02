0c431b024a1543674b40642b84bbacec0fdbc30d
sunil-promineo-FE <sunilvaishali43@gmail.com>
2025-04-02 16:45:25 +0530

    tmp commit
    tmp commit

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ require('dotenv').config({
 });
 
 module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/productionDB',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/',
   FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
   API_PREFIX: process.env.API_PREFIX || '/api',
   PORT: process.env.PORT || 3003,
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./config');
 const productRoutes = require('./routes/productRoutes');
-const mongoose = require('mongoose');
 
 const app = express();
 
