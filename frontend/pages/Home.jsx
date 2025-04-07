b41e4b3db1eb5489218ea8c91aa23d09dbcc9c0b
sunil-promineo-FE <sunilvaishali43@gmail.com>
2025-04-07 23:26:18 +0530

    Update Home.jsx

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -20,18 +20,49 @@ const Home = () => {
     <div>
       <Header />
       <Banner />
-		<Container fluid className="px-0">
-		  <Row className="g-0">
-		    <Col md={4} className="ps-0"> {/* Increase width of SidePanel */}
-		      <SidePanel onCategoryChange={handleCategoryChange} />
-		    </Col>
-		    <Col md={8}> {/* Decrease width of ProductGrid */}
-		      <ProductGrid selectedCategory={selectedCategory} selectedSubcategory={selectedSubcategory} />
-		    </Col>
-		  </Row>
-		</Container>
+      <Container fluid className="px-0">
+        <Row className="g-0">
+          <Col md={3} className="ps-0">
+            <SidePanel onCategoryChange={handleCategoryChange} />
+          </Col>
+          <Col md={9}>
+            <ProductGrid selectedCategory={selectedCategory} selectedSubcategory={selectedSubcategory} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
 
 export default Home;
+/*16 Mar 25
+
+//src/pages/Home.jsx
+import React from 'react';
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import ProductGrid from '../components/ProductGrid';
+import SidePanel from '../components/SidePanel';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Container fluid className="px-0">
+        <Row className="g-0">
+          <Col md={3} className="ps-0">
+            <SidePanel />
+          </Col>
+          <Col md={9}>
+            <ProductGrid />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
+*/
